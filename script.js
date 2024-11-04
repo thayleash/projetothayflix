b65b26ae3b032e0501694552802f3e087eb894b2
EduSantos <edu.bio.uerj@gmail.com>
2024-11-03 21:38:44 -0300

    Add files via upload
    Projeto de pagina da Netflix para aprendizado.

---
+++ script.js
@@ -0,0 +1,29 @@
+let botaoSom = document.querySelector(".botao-som")
+let video = document.querySelector(".video")
+let botao = document.querySelector(".link-info")
+let modal = document.querySelector(".modal")
+let audio = document.querySelector(".audio")
+
+botaoSom.addEventListener("click", ligaSom)
+
+function ligaSom() {
+    video.muted =  !video.muted
+}
+
+
+botao.addEventListener("click", mostrarModal)
+modal.addEventListener("click", esconderModal)
+
+function mostrarModal() {
+    modal.style.display = "block"
+}
+
+function esconderModal() {
+    modal.style.display = "none"
+}
+
+window.addEventListener("load", tocarAudio)
+
+function tocarAudio() {
+    audio.play()
+}
